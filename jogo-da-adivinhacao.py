96e8857e6e45fde479b7d0513836bba10bcbc82a
Lahlops21 <laisllopess21@gmail.com>
2025-02-19 17:11:22 -0300

    primeiro commit jogo da adivinhação

---
+++ jogo-da-adivinhacao.py
@@ -0,0 +1,24 @@
+print('Estou pensando em um número entre 0 e 15... ')
+print('Quero ver se consegue adivinhar qual foi...')
+print('='* 40)
+print('')
+from random import randint
+computador = randint(0,15)
+
+palpite = 0
+acertou = False
+
+while acertou == False:
+  jogador = int(input('Escolha um número entre 0 e 15: '))
+  palpite += 1
+  if jogador == computador:
+    acertou = True
+  else:
+    if jogador > computador:
+      print('O número é menor... Tente outra vez!')
+    elif jogador < computador:
+      print('O número é maior... Tente outra vez!')
+print(f'Acertou com {palpite} tentativas!')
+
+
+
